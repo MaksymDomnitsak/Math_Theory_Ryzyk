96b2957731cdcf8aa733245ba97ad3a10e7aa2ed
maximuse.domn <user.maximuse@gmail.com>
2021-12-05 20:27:06 +0200

    Add files via upload

---
+++ Lab5/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace Lab5
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvIncome = new System.Windows.Forms.DataGridView();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnBayes = new System.Windows.Forms.Button();
+            this.lblBayes = new System.Windows.Forms.Label();
+            this.btnSV = new System.Windows.Forms.Button();
+            this.lblSV = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvIncome)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvIncome
+            // 
+            this.dgvIncome.AllowUserToDeleteRows = false;
+            this.dgvIncome.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvIncome.Location = new System.Drawing.Point(53, 79);
+            this.dgvIncome.Name = "dgvIncome";
+            this.dgvIncome.RowTemplate.Height = 25;
+            this.dgvIncome.Size = new System.Drawing.Size(343, 291);
+            this.dgvIncome.TabIndex = 0;
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(550, 127);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 43);
+            this.btnRead.TabIndex = 1;
+            this.btnRead.Text = "Read Incomes";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnBayes
+            // 
+            this.btnBayes.Enabled = false;
+            this.btnBayes.Location = new System.Drawing.Point(440, 228);
+            this.btnBayes.Name = "btnBayes";
+            this.btnBayes.Size = new System.Drawing.Size(75, 47);
+            this.btnBayes.TabIndex = 2;
+            this.btnBayes.Text = "Bayes criteria";
+            this.btnBayes.UseVisualStyleBackColor = true;
+            this.btnBayes.Click += new System.EventHandler(this.btnBayes_Click);
+            // 
+            // lblBayes
+            // 
+            this.lblBayes.AutoSize = true;
+            this.lblBayes.Location = new System.Drawing.Point(440, 302);
+            this.lblBayes.Name = "lblBayes";
+            this.lblBayes.Size = new System.Drawing.Size(38, 15);
+            this.lblBayes.TabIndex = 3;
+            this.lblBayes.Text = "label1";
+            this.lblBayes.Visible = false;
+            // 
+            // btnSV
+            // 
+            this.btnSV.Enabled = false;
+            this.btnSV.Location = new System.Drawing.Point(648, 228);
+            this.btnSV.Name = "btnSV";
+            this.btnSV.Size = new System.Drawing.Size(75, 47);
+            this.btnSV.TabIndex = 4;
+            this.btnSV.Text = "Min SV criteria";
+            this.btnSV.UseVisualStyleBackColor = true;
+            this.btnSV.Click += new System.EventHandler(this.btnSV_Click);
+            // 
+            // lblSV
+            // 
+            this.lblSV.AutoSize = true;
+            this.lblSV.Location = new System.Drawing.Point(648, 301);
+            this.lblSV.Name = "lblSV";
+            this.lblSV.Size = new System.Drawing.Size(38, 15);
+            this.lblSV.TabIndex = 5;
+            this.lblSV.Text = "label1";
+            this.lblSV.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblSV);
+            this.Controls.Add(this.btnSV);
+            this.Controls.Add(this.lblBayes);
+            this.Controls.Add(this.btnBayes);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.dgvIncome);
+            this.Name = "Form1";
+            this.Text = "Lab5";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvIncome)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvIncome;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnBayes;
+        private System.Windows.Forms.Label lblBayes;
+        private System.Windows.Forms.Button btnSV;
+        private System.Windows.Forms.Label lblSV;
+    }
+}
+
---
+++ Lab5/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab5
+{
+    public partial class Form1 : Form
+    {
+        double[,] Incomes;
+        double[] Probabs;
+        double[] Bcriteries;
+        public Form1()
+        {
+            InitializeComponent();
+            Incomes = new double[4, 5];
+            Probabs = new double[5];
+            Bcriteries = new double[4];
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            string filepath = "";
+            DataTable a = new DataTable();
+            DataRow r;
+            a.Columns.Add("θ1");
+            a.Columns.Add("θ2");
+            a.Columns.Add("θ3");
+            a.Columns.Add("θ4");
+            a.Columns.Add("θ5");
+            int i = 0;
+            string[] oneX = new string[5] { "=", "=", "=", "=", "=" };
+            string l = new string(" ");
+            OpenFileDialog openfile = new OpenFileDialog();
+            openfile.Filter = "Text files(*.txt)|*.txt";
+            if (openfile.ShowDialog() == DialogResult.OK)
+            {
+                filepath = openfile.FileName;
+
+                Stream fileStream = openfile.OpenFile();
+
+                using (StreamReader reader = new StreamReader(filepath))
+                {
+                    while (l != null)
+                    {
+                        l = reader.ReadLine();
+                        if (l == null)
+                        {
+                            break;
+                        }
+                        oneX = l.Split(" ");
+                        r = a.NewRow();
+                        r["θ1"] = oneX[0];
+                        r["θ2"] = oneX[1];
+                        r["θ3"] = oneX[2];
+                        r["θ4"] = oneX[3];
+                        r["θ5"] = oneX[4];
+                        a.Rows.Add(r);
+                        if (i < 4)
+                        {
+                            Incomes[i, 0] = Convert.ToDouble(oneX[0]);
+                            Incomes[i, 1] = Convert.ToDouble(oneX[1]);
+                            Incomes[i, 2] = Convert.ToDouble(oneX[2]);
+                            Incomes[i, 3] = Convert.ToDouble(oneX[3]);
+                            Incomes[i, 4] = Convert.ToDouble(oneX[4]);
+                            i++;
+                        }
+                        else {
+                            Probabs[0] = Convert.ToDouble(oneX[0]);
+                            Probabs[1] = Convert.ToDouble(oneX[1]);
+                            Probabs[2] = Convert.ToDouble(oneX[2]);
+                            Probabs[3] = Convert.ToDouble(oneX[3]);
+                            Probabs[4] = Convert.ToDouble(oneX[4]);
+                        }
+                    }
+                }
+                dgvIncome.DataSource = a;
+                fileStream.Close();
+                btnBayes.Enabled = true;
+            }
+        }
+
+        private void btnBayes_Click(object sender, EventArgs e)
+        {
+            double[] criteries = new double[4];
+            for(int i = 0; i < 4; i++)
+            {
+                criteries[i] = Incomes[i, 0] * Probabs[0] + Incomes[i, 1] * Probabs[1] + Incomes[i, 2] * Probabs[2] + Incomes[i, 3] * Probabs[3] + Incomes[i, 4] * Probabs[4];
+            }
+            criteries.CopyTo(Bcriteries,0);
+            lblBayes.Visible = true;
+            lblBayes.Text = "c1 = " + criteries[0] + "\nc2 = " + criteries[1] + "\nc3 = " + criteries[2] + "\nc4 = " + criteries[3] 
+                + "\nIndex of best choose: " + (Array.IndexOf(criteries, criteries.Max())+1);
+            btnSV.Enabled = true;
+        }
+
+        private void btnSV_Click(object sender, EventArgs e)
+        {
+            double[,] alphaCoefs = FindAlpha();
+            double[] probabCoefs = SumProbabCoefs(alphaCoefs);
+            double[] semivarCoefs = new double[4] { 0.0, 0.0, 0.0, 0.0 };
+            for (int i = 0; i < Incomes.GetUpperBound(0)+1; i++)
+            {
+                for(int j=0;j < Incomes.GetUpperBound(1)+1; j++)
+                {
+                    semivarCoefs[i] += alphaCoefs[i, j] * Probabs[j] * Math.Pow(Incomes[i, j] - Math.Round(Bcriteries[i],3), 2.0);
+                }
+                semivarCoefs[i] *= (1 / Math.Round(probabCoefs[i], 3));
+            }
+            lblSV.Visible = true;
+            lblSV.Text = "c1 = " + semivarCoefs[0] + "\nc2 = " + semivarCoefs[1] + "\nc3 = " + semivarCoefs[2] + "\nc4 = " + semivarCoefs[3]
+    + "\nIndex of best choose: " + (Array.IndexOf(semivarCoefs, semivarCoefs.Min()) + 1);
+
+        }
+
+        public double[,] FindAlpha()
+        {
+            double[,] alphas = new double[Incomes.GetUpperBound(0)+1, Incomes.GetUpperBound(1)+1];
+            for(int i = 0; i < 4; i++)
+            {
+                for(int j = 0; j < 5; j++)
+                {
+                    if (Bcriteries[i] > Incomes[i, j])
+                    {
+                        alphas[i, j] = 1;
+                    }
+                    else alphas[i, j] = 0;
+                }
+            }
+            return alphas;
+        }
+
+        public double[] SumProbabCoefs(double[,] alphas)
+        {
+            double[] sums = new double[alphas.GetUpperBound(0)+1];
+            for(int i = 0; i < 4; i++)
+            {
+                for(int j = 0; j < 5; j++)
+                {
+                    sums[i] += Probabs[j] * alphas[i, j];
+                }
+            }
+            return sums;
+        }
+    }
+}
